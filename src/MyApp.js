d73d91841d25918e295601ad9008c72154c39a50
Hikmatullah Mohammadi <hikmatullah.m80@gmail.com>
2022-04-12 11:54:10 +0430

    Continued

---
+++ src/MyApp.js
@@ -0,0 +1,11 @@
+import Main from './components/Main'
+
+const MyApp = () => {
+  return (
+    <>
+      <Main />
+    </>
+  )
+}
+
+export default MyApp
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+import MyApp from './MyApp'
+
+ReactDom.render(
+  <MyApp />,
+  document.getElementById('root')  
+)
