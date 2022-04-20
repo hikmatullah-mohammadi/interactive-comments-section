0285436b3b76020701ec16039d54ac8982edf07c
Hikmatullah Mohammadi <hikmatullah.m80@gmail.com>
2022-04-20 05:40:55 +0430

    Fixed delete alert box style issue

--- src/components/DeleteAlert.js
+++ src/components/DeleteAlert.js
@@ -13,19 +13,15 @@ const DeleteAlert = () => {
     dispatch(updateCreatedAtMsg())
   }
   return (
-    <div className='delete-alert'>
-      <div className='bg-alert' style={{ display: deleteCommentId > -1 ? 'grid' : 'none'}}>
-        <div>
-          <div className='title'>
-            Delete Comment
-          </div>
-          <div className='message'>
-            Are you sure you want to delete this comment? This will remove the comment and can't be undone.
-          </div>
-          <div className='response'>
-            <button onClick={handleCancel} >NO, CANCEL</button>
-            <button onClick={handleYes}>YES, DELETE</button>
-          </div>
+    <div className='delete-alert' style={{ display: deleteCommentId > -1 ? 'grid' : 'none'}}>
+      <div className="alert">
+        <h2 className='title'>Delete Comment</h2>
+        <p className='message'>
+          Are you sure you want to delete this comment? This will remove the comment and can't be undone.
+        </p>
+        <div className='response'>
+          <button onClick={handleCancel} >NO, CANCEL</button>
+          <button onClick={handleYes}>YES, DELETE</button>
         </div>
       </div>
     </div>
